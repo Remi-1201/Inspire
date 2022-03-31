00a5c78fbdd9e4a808437b13e12b4c36d9ee522a
remi-1201 <remi.nkjm@gmail.com>
2022-03-31 20:50:44 +0900

    Add title column to blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_051648) do
+ActiveRecord::Schema.define(version: 2022_03_31_114550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_03_31_051648) do
     t.text "detail", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   create_table "comments", force: :cascade do |t|
