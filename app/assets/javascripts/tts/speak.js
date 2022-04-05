56971d7c8b88967fc81d1e271913c880873f5bfc
remi-1201 <remi.nkjm@gmail.com>
2022-04-05 15:13:02 +0900

    fix blogs/index layout

--- app/assets/javascripts/tts/speak.js
+++ app/assets/javascripts/tts/speak.js
@@ -1,13 +1,13 @@
 $(document).on ("turbolinks:load", function(){
   // document.addEventListener('click',function(e){
-  //   var t=e.target;
-  //   if(t.id=="speak-btn"){
-  //     const text=Array.prototype.map.call(t.parentNode.parentNode.querySelectorAll('td'),function(n){
-  //       return n.textContent;
-  //     }).filter(function(v){
-  //       return v!=="";
-  //     })
-  //   };
+    // var t=e.target;
+    // if(t.id=="speak-btn"){
+    //   const a =Array.prototype.map.call(t.parentNode.parentNode.querySelectorAll('td'),function(n){
+    //     const text = n.textContent;
+    //   }).filter(function(v){
+    //     return v!=="";
+    //   })
+    // };
 
   const text        = document.querySelector('#detail')
   const voiceSelect = document.querySelector('#voice-select')
