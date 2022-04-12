e4bff41f917c798ace6973b23c23b05b046c410e
remi-1201 <remi.nkjm@gmail.com>
2022-04-12 10:27:32 +0900

    pauseと resumeボタンの追加

--- app/assets/javascripts/tts/speak.js
+++ app/assets/javascripts/tts/speak.js
@@ -5,6 +5,17 @@ $(document).on ("turbolinks:load", function(){
   function appendVoices() {
     const voices = speechSynthesis.getVoices()
     voiceSelect.innerHTML = ''
+    // for(i = 0; i < voices.length ; i++) {
+    //   var option = document.createElement('option');
+    //   option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    //   if(voices[i].default) {
+    //     option.textContent += ' -- DEFAULT';
+    //   }
+    
+    //   option.setAttribute('data-lang', voices[i].lang);
+    //   option.setAttribute('data-name', voices[i].name);
+
     voices.forEach(voice => {
       if(!voice.lang.match('ja|en-US')) return
       const option = document.createElement('option')
@@ -26,6 +37,14 @@ $(document).on ("turbolinks:load", function(){
       .getVoices()
       .filter(voice => voice.name === voiceSelect.value)[0]
     speechSynthesis.speak(uttr)
+  })
+
+  $('.pause-btn').on('click', function(){
+    speechSynthesis.pause();
+  })
+
+  $('.resume-btn').on('click', function(){
+    speechSynthesis.resume();
   })
 
   $('.stop-btn').on('click', function(){
