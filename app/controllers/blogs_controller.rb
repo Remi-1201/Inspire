a6adc4636aa4d1fc0f82de6f99e534e5fd268035
remi-1201 <remi.nkjm@gmail.com>
2022-04-15 18:00:45 +0900

    検索機能のデバッグ

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -62,14 +62,7 @@ class BlogsController < ApplicationController
   end
 
   def search    
-    @searched_blogs = Blog.search(params[:search])
-    @searched_blogs = @searched_blogs.page(params[:page])
-    # unless params[:search].nil?
-    #   if params[:search_detail].present? 
-    #     @blogs = Blog.where('detail LIKE ?', "%#{params[:search_detail]}%").order(created_at: :desc).kaminari(params[:page])
-    #   else
-    #   end
-    # end
+    @blogs = @blogs.page(params[:page])
     @blogs =
     if params[:search_title].blank? && params[:search_detail].blank?
       Blog.all.order(created_at: :desc).kaminari(params[:page])
