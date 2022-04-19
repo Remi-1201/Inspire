a1fd504efb3f847497ac7d1a1f3b0fa53c4a0833
remi-1201 <remi.nkjm@gmail.com>
2022-04-19 10:20:14 +0900

    録音機能のJavascript修正

--- app/assets/javascripts/tts/record.js
+++ app/assets/javascripts/tts/record.js
@@ -1,85 +1,86 @@
-document.addEventListener('DOMContentLoaded', () => {
-window.addEventListener("DOMContentLoaded",function(){
-    newRec=new Rec(
-     document.getElementById("Control"),
-     document.getElementById("MicOn"),
-     document.getElementById("RecStart"),
-     document.getElementById("RecStop"),
-     document.getElementById("RecMime")
-   );
- });
-  var Rec=function(control,micOnBtn,recStartBtn,recStopBtn,recMime){
-    this.control=control;
-    this.micOnBtn=micOnBtn;
-    this.recStartBtn=recStartBtn;
-    this.recStopBtn =recStopBtn;
+$(document).on('turbolinks:load page:load page:change ready', () => {
+  var Rec = function (control, micOnBtn, recStartBtn, recStopBtn, recMime) {
+    this.control = control;
+    this.micOnBtn = micOnBtn;
+    this.recStartBtn = recStartBtn;
+    this.recStopBtn = recStopBtn;
     this.recMime = recMime;
-    
-    this.stream=null;
-    this.mediaRecorder=null;
-    this.chunks=[];
-    this.micOnBtn.setAttribute("enabled",true);
-    this.recStartBtn.setAttribute("disabled",true);
-    this.recStopBtn.setAttribute("disabled",true);
-    this.type=null;
-    
-    this.micOnBtn.addEventListener("click",function(){
 
-      if(navigator.mediaDevices==undefined){
+    this.stream = null;
+    this.mediaRecorder = null;
+    this.chunks = [];
+    this.micOnBtn.setAttribute("enabled", true);
+    this.recStartBtn.setAttribute("disabled", true);
+    this.recStopBtn.setAttribute("disabled", true);
+    this.type = null;
+
+    this.micOnBtn.addEventListener("click", function () {
+
+      if (navigator.mediaDevices == undefined) {
         alert('Your browser does not support this function or you are not connected to the internet.');
         return;
       }
 
-      navigator.mediaDevices.getUserMedia({audio:true})
-      .then(function(stream){
-        this.stream=stream;        
-        this.mediaRecorder=new MediaRecorder(this.stream);
-        this.mediaRecorder.addEventListener("dataavailable",function(event){
-          this.chunks.push(event.data);
-          console.log(event.data);
-        }.bind(this));
-        this.mediaRecorder.addEventListener("stop",function(e){
-          this.type=this.chunks[0].type;
-          this.recMime.innerHTML=this.type;
-          let blob=new Blob(this.chunks,{"type":this.type});
-          this.chunks=[];
-  
-          let fileReaderAudio=new FileReader();
-          fileReaderAudio.addEventListener("load",function(event){
-            let audio_play=document.body.querySelector("#audio_play");
-            if(audio_play==null){
-              audio_play=document.createElement("audio");
-              audio_play.setAttribute("controls","true");
-              audio_play.setAttribute("id","audio_play");
-              audio_play.setAttribute("playsinline","");
-              this.control.appendChild(audio_play);
-            }else{
-              audio_play.pause();
-              audio_play.currentTime=0;
-            }
-            audio_play.setAttribute("src",event.target.result);
-            audio_play.load();
-            audio_play.play();
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(function (stream) {
+          this.stream = stream;
+          this.mediaRecorder = new MediaRecorder(this.stream);
+          this.mediaRecorder.addEventListener("dataavailable", function (event) {
+            this.chunks.push(event.data);
+            console.log(event.data);
           }.bind(this));
-          fileReaderAudio.readAsDataURL(blob);
-  
+          this.mediaRecorder.addEventListener("stop", function (e) {
+            this.type = this.chunks[0].type;
+            this.recMime.innerHTML = this.type;
+            let blob = new Blob(this.chunks, { "type": this.type });
+            this.chunks = [];
+
+            let fileReaderAudio = new FileReader();
+            fileReaderAudio.addEventListener("load", function (event) {
+              let audio_play = document.body.querySelector("#audio_play");
+              if (audio_play == null) {
+                audio_play = document.createElement("audio");
+                audio_play.setAttribute("controls", "true");
+                audio_play.setAttribute("id", "audio_play");
+                audio_play.setAttribute("playsinline", "");
+                this.control.appendChild(audio_play);
+              } else {
+                audio_play.pause();
+                audio_play.currentTime = 0;
+              }
+              audio_play.setAttribute("src", event.target.result);
+              audio_play.load();
+              audio_play.play();
+            }.bind(this));
+            fileReaderAudio.readAsDataURL(blob);
+
+            this.recStartBtn.removeAttribute("disabled");
+          }.bind(this));
           this.recStartBtn.removeAttribute("disabled");
-          this.recStopBtn.setAttribute("disabled",true);
+          this.recStopBtn.removeAttribute("disabled");
+
+          this.micOnBtn.setAttribute("disabled", true);
+        }.bind(this)).catch(function (e) {
+          console.log(e);
+          document.getElementById("alert").innerHTML = e;
         }.bind(this));
-        this.recStartBtn.removeAttribute("disabled");
-        this.micOnBtn.setAttribute("disabled",true);
-      }.bind(this)).catch(function(e){
-        console.log(e);
-        document.getElementById("alert").innerHTML=e;
-      }.bind(this));
     }.bind(this));
-    this.recStartBtn.addEventListener("click",function(){
+    this.recStartBtn.addEventListener("click", function () {
       this.recStartBtn.setAttribute("disabled", true);
       this.recStopBtn.removeAttribute("disabled");
       this.mediaRecorder.start();
-    }.bind(this));    
-    this.recStopBtn.addEventListener("click",function(){
-      this.mediaRecorder.stop();      
     }.bind(this));
-  }  
- }); 
+    this.recStopBtn.addEventListener("click", function () {
+      this.mediaRecorder.stop();
+      document.getElementById("Control").style.display = "block";
+    }.bind(this));
+  }
+
+  newRec = new Rec(
+    document.getElementById("Control"),
+    document.getElementById("MicOn"),
+    document.getElementById("RecStart"),
+    document.getElementById("RecStop"),
+    document.getElementById("RecMime")
+  );
+})
